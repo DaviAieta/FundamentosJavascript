355fb8168d87ec7665fd5fa625b530369af9497c
DaviAieta <aieta.davi@gmail.com>
2024-01-16 14:16:42 -0300

    Create & Update new files

---
+++ TiposPrimitivos/array.js
@@ -0,0 +1,9 @@
+// Lista de frutas usando -> []
+frutas = ['Banana', 'Uva', 'Morango']
+
+frutas.forEach(element => {
+    console.log(element)
+})
+
+// Tamanho/qtde de elementos dentro do array
+console.log(frutas.length)
---
+++ TiposPrimitivos/objects.js
@@ -0,0 +1,21 @@
+let tenis = {
+    cadarco: {
+        cor: 'Preto',
+        tamanho: 'M'
+    },
+    preco: 150,
+    disponivel: true,
+    estoque: 4,  
+}
+
+let tenis2 = {
+    cadarco: {
+        cor: 'Preto',
+        tamanho: 'M'
+    },
+    preco: 150,
+    disponivel: true,
+    estoque: 4,
+}
+
+console.log('tenis1:', tenis, 'tenis2:',tenis2)
