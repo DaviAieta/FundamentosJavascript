c0046516adfaedf156eb3630810cc170b5157b01
DaviAieta <aieta.davi@gmail.com>
2024-01-16 18:49:54 -0300

    Create new files

---
+++ Objetos/index.js
@@ -0,0 +1,21 @@
+let Tenis = {
+    tipo: 'Corrida',
+    cadarco:{
+        cor: 'preto',
+        tamanho: 'M',
+    },
+    tamanho:{
+        tenis: 40,
+        palmilha: 38,
+        caixa:{
+            altura: 35,
+            largura: 40,
+            profundidade: 10
+        }
+    },
+    estoque: 9,
+    marca: 'Nike'
+}
+
+// Acessando valores do objeto
+console.log(Tenis)
---
+++ Objetos/metodos.js
@@ -0,0 +1,9 @@
+const tenis = (tamanho, estoque, preco) =>{
+    return{
+        tamanho: tamanho,
+        estoque: estoque,
+        preco: preco
+    }
+}
+
+console.log(tenis(35, false, 140))
